a54fade48de0e2a1d077225ddc132ef5d3b621f9
施能 <shineng@staff.zjutv.com>
2018-01-25 14:38:44 +0800

    finish hw1

--- PS1/q2/lwlr.m
+++ PS1/q2/lwlr.m
@@ -1,3 +1,29 @@
 function y = lwlr(X_train, y_train, x, tau)
 
 %%% YOUR CODE HERE
+	%%constants 
+	epsilon = 1e-5;
+	ITER_MAX = 20;
+	lambda = 0.0001;
+
+	m = size(X_train, 1);
+	n = size(X_train, 2);
+
+	theta = zeros(n, 1);
+
+	%compute weights
+	w = exp(-sum((X_train - repmat(x',m,1)).^2, 2)/(2*tau));
+  g = ones(n,1); 
+
+	while (norm(g)>epsilon)
+		h = 1./(1+exp(-X_train*theta));
+		g = X_train'*(w.*(y_train - h)) - lambda.*theta;
+		D = diag(-w.*h.*(1-h));
+		H = X_train'*D*X_train - lambda*eye(n);
+		theta = theta -  H \ g;
+	end
+
+	y = x'*theta > 0;
+
+
+end 
